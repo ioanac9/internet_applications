91407eb1d9247534d65bfd0845346bcab513d7d4
Ioana Constantinescu <47981297+ioanac9@users.noreply.github.com>
2022-01-15 16:46:37 +0100

    ?

---
+++ public_html/recommandations.php
@@ -0,0 +1,15 @@
+<?php
+	try {
+		$ruta = "/home/alumnos/ai54/public_html/octave\r\n";
+		$fun = "updateRecomendation(".$_COOKIE['sesion'].")\r\n";
+		$info = $ruta.$fun.chr(0);
+
+		$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+		socket_connect($socket, "127.0.0.1", 1111);
+		$sent = socket_write($socket, $info, strlen($info));
+
+		echo "<script>document.location = 'myAccount.php'</script>";
+	} catch (Exception $e) {
+		echo $e->getMessage();
+	}
+?>
