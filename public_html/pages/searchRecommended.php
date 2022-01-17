24201e6f8ea4d2311fbf8bdfcadec93ac5939360
Gal Oscar <oscar.gal@rodeapps.com>
2022-01-17 22:56:37 +0100

    updatedSearch recomanded

--- public_html/pages/searchRecommended.php
+++ public_html/pages/searchRecommended.php
@@ -1,11 +1,56 @@
 <html>
 <head>
-<title>TVTIME</title>
-<link rel="stylesheet" type="text/css" href="../styling/styles.css">
+    <title>My Search Recommendations</title>
+
+    <!--  Meta Data  -->
+    <meta charset="UTF-8">
+    <meta name="description" content="My account page. You can update your account and also delete it">
+    <meta name="keywords" content="HTML, CSS, JavaScript, php">
+    <meta name="author" content="Oscar Gal | Ioana Constantinescu">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+    <!--  CSS  -->
+    <link rel="stylesheet" type="text/css" href="../styling/styles.css">
+
+    <!--  Fonts  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@200&family=Poppins:wght@300&display=swap" rel="stylesheet">
+
+    <!--  JS  -->
+    <script type='text/javascript'>
+        function remove(){
+            const message = window.confirm('Are you sure you want to delete your account?');
+            if (message){
+                window.location = '../scripts/deleteAccount.php';
+            }else{
+                window.location = 'myAccount.php';
+            }
+
+        }
+    </script>
 </head>
 <body>
-<h1><a href = "main.php">TVT</a></h1>
-<h1 align='center'>Recommended movies </h1>
+<div class="nav-bar">
+    <a href='pages/main.php'>
+        <img class='logo' src="../images/logo_movie.png" alt="logo of the company.">
+    </a>
+    <div class='nav-bar-menu'>
+        <?php
+        if (isset($_COOKIE['session'])) {
+            echo "<a class='submit-btn' href='myAccount.php'>My account</a>";
+            echo "<form action='../scripts/logout.php' method='GET'>";
+            echo "<input class='submit-btn' type='submit' name='exit' value='Logout'/>";
+            echo "</form>";
+        } else {
+            echo "<a class='submit-btn' href='../index.html'>Login/Register</a>";
+        }
+        ?>
+    </div>
+</div>
+
+<h1 class="margin-top-menu">Recommended movies </h1>
 
 <?php
         try {
