19ce2fe205ba11ceed2fde95511ae77ef7711ec5
Ioana Constantinescu <47981297+ioanac9@users.noreply.github.com>
2022-01-22 21:17:34 +0100

    rename2

--- public_html/recommandations.php
+++ public_html/recommandations.php
@@ -1,8 +1,8 @@
 <?php
 	try {
-		$ruta = "/home/alumnos/ai54/public_html/octave\r\n";
-		$fun = "updateRecomendation(".$_COOKIE['session'].")\r\n";
-		$info = $ruta.$fun.chr(0);
+		$route = "/home/alumnos/ai54/public_html/octave\r\n";
+		$update = "updateRecomendation(".$_COOKIE['session'].")\r\n";
+		$info = $route.$update.chr(0);
 
 		$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 		socket_connect($socket, "127.0.0.1", 1111);
